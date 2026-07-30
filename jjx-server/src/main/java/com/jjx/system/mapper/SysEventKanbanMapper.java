92c06f5ab764e76c15862e973971ab1cebe46fa9
Administrator <541124712@qq.com>
2026-07-31 01:09:38 +0800

    cleanup(event): 删除旧事件配置表和对应的Java实体/Mapper
    - 删表: sys_event_config_old, sys_event_kanban, sys_event_notification, sys_notification_template
    - 删文件: SysEventKanban.java, SysEventNotification.java, SysEventKanbanMapper.java, SysEventNotificationMapper.java
    - 通知/任务统一由新 sys_event_config 单表驱动

--- jjx-server/src/main/java/com/jjx/system/mapper/SysEventKanbanMapper.java
+++
@@ -1,6 +0,0 @@
-package com.jjx.system.mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.jjx.system.domain.entity.SysEventKanban;
-import org.apache.ibatis.annotations.Mapper;
-@Mapper
-public interface SysEventKanbanMapper extends BaseMapper<SysEventKanban> {}
--- jjx-server/src/main/java/com/jjx/system/mapper/SysEventNotificationMapper.java
+++
@@ -1,6 +0,0 @@
-package com.jjx.system.mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.jjx.system.domain.entity.SysEventNotification;
-import org.apache.ibatis.annotations.Mapper;
-@Mapper
-public interface SysEventNotificationMapper extends BaseMapper<SysEventNotification> {}
